9c40bcd157f07197f69b25d34f09450d23d2e2b6
Nastasjia Plakhtii <n.plakhtii@gmail.com>
2020-11-16 19:25:36 +0200

    Add main components (footer, header, content)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  IconDefinition,
+  faPhoneAlt,
+  faMailBulk,
+  faPhoneSquareAlt,
+  faMapMarkedAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope,
+} from '@fortawesome/free-regular-svg-icons';
+import { MainComponent } from './main/main.component';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+
+const icons: IconDefinition[] = [
+  faPhoneAlt,
+  faEnvelope,
+  faMailBulk,
+  faPhoneSquareAlt,
+  faMapMarkedAlt,
+  faFacebookSquare,
+];
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ...materialModules,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(...icons);
+  }
+}
